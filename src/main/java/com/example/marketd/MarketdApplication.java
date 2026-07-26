3f2165c9d1db467aa4a0adf2130aab0b49e4bf84
EduardoDev0706 <eduardodev2026@gmail.com>
2026-07-26 17:40:54 -0300

    chore: initial commit from Spring Initializr

---
+++ src/main/java/com/example/marketd/MarketdApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marketd/MarketdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
